cd689268b3c3bb56e3f36306f5584bd61de9f4d7
rlandjon <rlandjon@gmail.com>
2020-02-20 15:45:57 +0800

    udpproxy:Update license infomation.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # udpproxy
-UDP转发程序，把FFMPEG按时间片切割的HLS序列发送到指定的IP及端口地址，程序包含了友好的log及config读取功能，利用linux内核提供的list_head双链表高效完成功能，也包含了对inotify对文件夹监控的功能。
+UDPProxy sends FFMPEG time-sliced HLS sequence to 
+the specified IP and port address. The program contains 
+the friendly log and config reading functions, use the 
+list_head double-linked list function provided by the 
+Linux kernel, and also includes the function of inotify 
+for folder monitoring.
--- list.h
+++ list.h
@@ -1,4 +1,22 @@
-﻿#ifndef __LIST_H
+﻿/* Copyright (c) 2015-2019 rlandjon <rlandjon@gmail.com>
+ *
+ * This file is part of udpproxy.
+ *
+ * udpproxy is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+        * License as published by the Free Software Foundation; either
+        * version 2.1 of the License, or (at your option) any later version.
+ *
+ * udpproxy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+        * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with ijkPlayer; if not, write to the Free Software
+        * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+#ifndef __LIST_H
 #define __LIST_H
 
 /* This file is from Linux Kernel (include/linux/list.h)
